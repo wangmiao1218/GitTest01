3779519c1e0a9b1b8001af9727e228320662982b
wangmiao <wangmiao1218@163.com>
2016-09-18 19:34:33 +0800

    Hello World02
    Hello World02

--- src/com/atguigu/github/test/HelloWorld.java
+++ src/com/atguigu/github/test/HelloWorld.java
@@ -3,5 +3,6 @@ package com.atguigu.github.test;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Hello World02");
 	}
 }
